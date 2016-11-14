19bacbcbc206e397110a31850a5a4baf0782ef3c
esquivaflechas <lesims.g@gmail.com>
2016-11-14 12:59:31 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -3,5 +3,12 @@ ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
 	
+	var miNombre;
+	var miEdad;
+ 	miNombre = document.getElementById('elNombre').value;
+	miEdad = document.getElementById('laEdad').value;
+	alert("Usted se llama "+miNombre +" y tiene "+ miEdad+" anios");
+
+
 }
 
