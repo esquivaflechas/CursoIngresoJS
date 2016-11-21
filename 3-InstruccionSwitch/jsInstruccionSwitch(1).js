be260d345663ce0134fef769e9605d7b5277bce8
esquivaflechas <lesims.g@gmail.com>
2016-11-21 10:22:45 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,21 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-
+switch(mesDelAño){
+    case "Enero":
+        alert("Que comiences bien el año!");
+        break;
+    case "Marzo":
+        alert("A clases!");
+        break;
+    case "Julio":
+        alert("Se vienen las vacaciones!");
+        break;
+    case "Diciembre":
+        alert("Felices fiestas!!");
+        break;
+    
+} //Cierro switch(mesDelAño)
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,35 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+	{	
+		case "Enero":
+			alert("Mes de 31 dias.");
+			break;
+		case "Febrero":
+			alert("Mes de 28 dias.");
+			break;
+		case "Marzo":
+			alert("Mes de 31 dias.");
+			break;
+		case "Mayo":
+			alert("Mes de 31 dias.");
+			break;
+		case "Julio":
+			alert("Mes de 31 dias.");
+			break;
+		case "Agosto":
+			alert("Mes de 31 dias.");
+			break;
+		case "Octubre":
+			alert("Mes de 31 dias.");
+			break;
+		case"Diciembre":
+			alert("Mes de 31 dias.");
+			break;
+		default:
+			alert("Mes de 30 dias.");
+	} //Fin del switch
 
 
 
