3276c4458c0adcee5fffb5b9308e77877c8bc7e9
esquivaflechas <lesims.g@gmail.com>
2016-11-21 10:22:24 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota = Math.floor((Math.random() * 10) + 1);
+	if (nota == 9 || nota == 10) 
+	{
+		//document.getElementById('FormIngreso').value = "Excelente";	
+		alert("Excelente.");
+	} //Cierro if
+	else
+	{
+		if (nota >= 4 && nota < 9) 
+		{
+			//document.getElementById('FormIngreso').value = "Aprobo";
+			alert("Aprobo.");
+		} //Cierro if
+		else
+		{
+			//document.getElementById('FormIngreso').value = "Vamos, la proxima se puede";			
+			alert("Vamos, la proxima se puede.");
+		} //Cierro else
+	} //Cierro else
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,7 +2,9 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var numero = Math.floor((Math.random() * 10) + 1);
+	//document.getElementById('FormIngreso').value = numero;
 	alert("El numero es: " + numero);
+
 }
 
 //FIN DE LA FUNCIÓN
